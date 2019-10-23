cc078b6b1aca9737ff397b2c9bc7e7b5e9dae421
liucd <2420510778@qq.com>
2019-10-23 11:21:51 +0800

    测试

--- assets/Scipts/GameManager.ts
+++ assets/Scipts/GameManager.ts
@@ -32,6 +32,7 @@ export class GameManager extends Component {
     start () {
         this.curState = GameState.GS_INIT;
         this.playerCtrl.node.on('JumpEnd', this.onPlayerJumpEnd, this);
+        console.log("test");
     }
 
     init() {
